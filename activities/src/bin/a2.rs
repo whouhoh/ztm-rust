6a15d5939b477e561d7243c69c7fb1ec4bc9be0a
MarkwSch <141796429+whouhoh@users.noreply.github.com>
2023-12-24 16:42:50 +0800

    feat: completed activity a2

--- activities/src/bin/a2.rs
+++ activities/src/bin/a2.rs
@@ -8,4 +8,30 @@
 // * Use a function to display the result
 // * Use the "{:?}" token in the println macro to display the result
 
-fn main() {}
+// * Use a function to add two numbers together
+fn add(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+// * Use a function to display the result
+
+fn display(a: i32) {
+    // Print the results to the terminal
+    println!("{:?}", a);
+}
+
+// * Use the "{:?}" token in the println macro to display the result
+fn main() {
+    // Define num_1 and num_2
+    let num_1 = 8;
+    let num_2 = 9;
+    
+    // Run the add function with num_1 and num_2
+    let result = add(num_1, num_2);
+    display(result);
+}
+
+
+
+
+
